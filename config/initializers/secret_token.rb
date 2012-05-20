ac0e4a2fbc044a5862ab0f1c4b4c8305864d47bb
Oscar Escalada <oscar@webanddesignstudio.com>
2012-05-19 22:32:56 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perforchestor::Application.config.secret_token = '5585344e5b34865a93ec7db82f7195051345a674d012eec24517511007c53abf18e308be1a1dd5172ca9bd6b0c262af0f523e91c765453eecc925a3dc9cb541f'
