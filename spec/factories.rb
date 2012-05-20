ae95e5e4edc855cabac6757f50f9bd74a7f10ae8
Oscar Escalada <oscar@webanddesignstudio.com>
2012-05-20 12:34:11 -0700

    Sign Up for  completed

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Oscar Escalada"
+    email    "oscar@webanddesignstudio.com"
+    password "test123"
+    password_confirmation "test123"
+  end
+end
